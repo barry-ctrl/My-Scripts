78df6d3890f3fd196b6ebe1cb6b89c6e6796d813
Abrar <140312789+barry-ctrl@users.noreply.github.com>
2025-03-21 22:31:35 +0500

    Update fvs.sh

--- fvs.sh
+++ fvs.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # wrapper script for preview searching files using fzf
+
+# usage
+# fvs <dir u want to search>
+# if path not specified, present working directory will be searched
+
 # fzf program is a prerequisite for this
 # sudo apt install fzf
+# make sure to give executable permission to this script ->  chmod u+x fvs.sh
+# and place this file inside any directory listed in your PATH environment variable to run it from your present working directory, no need to ./fvs.sh 
 
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ]; then 
 	fzf --reverse --preview "cat {}"
 	exit 1
-elif [ $# -eq 1 ]; then
+elif [ $# -eq 1 ]; then 
 	dir_path=$1
 	if [ -d "$dir_path" ]; then
 		fzf --reverse --walker-root=$dir_path --preview "cat {}"
